ff96786984724b78cf15c970e046b8dcc41791fe
Daniel Court <dwcourt@gmail.com>
2023-03-31 15:08:02 +0100

    incremental regeneration

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export async function getStaticProps() {
     props: {
       work: items,
     },
+    revalidate: 1,
   };
 }
 
--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -35,6 +35,7 @@ export async function getStaticProps({ params }) {
     props: {
       work: items[0],
     },
+    revalidate: 1,
   };
 }
 
